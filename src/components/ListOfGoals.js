bd0117cedd2ecd41683a48ad86d2a5c43d970ee7
Feba Rachel Lalu <114395573+Fe195@users.noreply.github.com>
2023-03-14 07:51:40 +0530

    Update ListOfGoals.js

--- src/components/ListOfGoals.js
+++ src/components/ListOfGoals.js
@@ -1,13 +1,16 @@
 import React from "react";
 
-const ListOfGoals = () => {
-    return (
-      <ul>
-        <li>
-            <p>My goal is to goalName, by goalDeadline.</p>
-          </li>
-      </ul>
-    );
+const ListOfGoals = (props) => {
+  return (
+    <ul>
+      {
+        props.allGoals.map((ele, ind) => (<li key={ind}>
+          <p>My goal is to {ele.goal},by {ele.by}.</p>
+        </li>
+        ))
+      }
+    </ul>
+  );
 }
 
 export default ListOfGoals;
